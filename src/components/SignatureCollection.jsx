2c19b0a7b9fc1c45264a6e2ef1048a334ea9dc68
Nihal mohamed <73734438+Nihalmohamed-10@users.noreply.github.com>
2025-05-09 19:44:17 +0530

    detailPageCompleted

--- src/components/SignatureCollection.jsx
+++ src/components/SignatureCollection.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import demoimg from "../../src/assets/images/demoPerfume.png";
-
+import {Link} from "react-router-dom"
 const products = [
   { name: "Angels", price: "From Rs. 600.00" },
   { name: "Ember 23", price: "From Rs. 400.00" },
   {
+    id:1,
     name: "Exotic Musk",
     price: "From Rs. 800.00",
     rating: 4,
     reviews: 7,
     label: "Sale",
   },
-  { name: "Flora", price: "From Rs. 400.00" },
+  { id:2, name: "Flora", price: "From Rs. 400.00" },
   {
+    id:3,
     name: "Great Oud",
     price: "From Rs. 600.00",
     rating: 5,
     reviews: 1,
   },
   {
+    id:4,
     name: "Imperial valley",
     price: "From Rs. 800.00",
     rating: 5,
     reviews: 3,
   },
-  { name: "L Homme", price: "From Rs. 600.00" },
+  {id:5, name: "L Homme", price: "From Rs. 600.00" },
   {
+    id:6,
     name: "Legend",
     price: "From Rs. 300.00",
     rating: 4,
@@ -47,19 +51,21 @@ function SignatureCollection() {
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-0 gap-y-10 text-center">
       {products.map((product, idx) => (
           <div key={idx}>
-            <div className="relative w-full h-48">
-              <img
-                src={demoimg}
-                alt={product.name}
-                className="w-full h-full object-contain"
-              />
-              {/* Label like "Sale" */}
-              {/* {product.label && (
-                <span className="absolute bottom-2 left-2 bg-black text-white text-xs px-2 py-1 rounded">
-                  {product.label}
-                </span>
-              )} */}
-            </div>
+            <Link to= {`/detail`}>
+              <div className="relative w-full h-48">
+                <img
+                  src={demoimg}
+                  alt={product.name}
+                  className="w-full h-full object-contain"
+                />
+                {/* Label like "Sale" */}
+                {/* {product.label && (
+                  <span className="absolute bottom-2 left-2 bg-black text-white text-xs px-2 py-1 rounded">
+                    {product.label}
+                  </span>
+                )} */}
+              </div>
+            </Link>
 
             <p className=" mt-2 font-medium px-2">{product.name}</p>
 
